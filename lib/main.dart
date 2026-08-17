166991bc2e8e746a70aad55c6071d259cce805d7
Gael Omar <PalaciosOmar2126@outlook.es>
2026-08-16 23:47:36 -0600

    git ignore activo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'screens/splash_screen.dart';
 import 'utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp();
   await AppThemeController.init();
   runApp(const MyApp());
--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'weather_model.dart'; // Se conecta con el archivo del Paso 2
 
 class WeatherService {
-  final String apiKey = '8ee9f2f40f707f4706714371966ad607'; 
+  final String apiKey = dotenv.env['WEATHER_API_KEY'] ?? '';
 
   Future fetchWeather(String city) async {
     final String url = 'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$apiKey&units=metric&lang=es';
